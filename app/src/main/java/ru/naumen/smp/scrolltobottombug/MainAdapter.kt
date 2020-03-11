ef6934c4baa8569b3bc67e9d0a8b7f66ecd288d2
Andrey Chernoprudov <dinloq@gmail.com>
2020-03-11 17:08:39 +0500

    add identifier

--- app/src/main/java/ru/naumen/smp/scrolltobottombug/MainAdapter.kt
+++ app/src/main/java/ru/naumen/smp/scrolltobottombug/MainAdapter.kt
@@ -3,11 +3,14 @@ package ru.naumen.smp.scrolltobottombug
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class MainAdapter : RecyclerView.Adapter<MainAdapter.Holder>() {
 
-    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView by lazy { itemView.findViewById<TextView>(R.id.title_text) }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val inflater = LayoutInflater.from(parent.context)
@@ -18,5 +21,6 @@ class MainAdapter : RecyclerView.Adapter<MainAdapter.Holder>() {
     override fun getItemCount(): Int = 30
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.textView.text = "Item $position"
     }
 }
